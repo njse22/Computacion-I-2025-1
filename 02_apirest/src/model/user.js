fab9eec1b7114622901ba8f556eeaed6037d37e7
njse22 <njse22@gmail.com>
2025-04-22 10:41:13 -0500

    update

--- 02_apirest/src/model/user.js
+++ 02_apirest/src/model/user.js
@@ -1,5 +1,12 @@
 class User {
     constructor(name, email){
+	console.log("USER")
+	console.log(name)
+	console.log(!name)
+
+	console.log(email)
+	console.log(!email)
+
 	if(!name || !email){
 	    throw new Error("Name and Email can not be empty")
 	}
